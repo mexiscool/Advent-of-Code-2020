2dfa0f1a8e609acc13d4c3492ec2b4c1260a9c0f
mexiscool <mexiscool@gmail.com>
2020-12-15 08:18:07 +0100

    small improvements

--- day 15 (130 min)/solver2.py
+++ day 15 (130 min)/solver2.py
@@ -1,30 +1,21 @@
-
 a =[int(i) for i in open('input.txt', 'r').read().split(',')]
-#b = a*(int(2020/len(a)+1))
 cnt = len(a)
 dict = {}
 for i,j in enumerate(a):
 	dict[j] = i
-last2 = a[-2]
 last = a[-1]
 while cnt <30000000:
 	try:
 		idx=dict[last]
 		#print(last,idx, cnt, dict[0])
-		last2, last = last, cnt-1-idx
-		dict[last2] = cnt-1
-
+		dict[last] = cnt-1
+		last = cnt-1-idx
 	except:
 		#print('ehm',last, last2)
-		last2, last = last, 0
-		dict[last2] = cnt-1
-		
-		
-
+		dict[last] = cnt-1
+		last = 0
 	cnt +=1
-	if cnt%100000==0:
-		print(cnt)
+	#if cnt%100000==0:
+	#	print(cnt)
 	#print(a[0:-1][::-1].index(j))
-#print(a)
-
 print(last)
